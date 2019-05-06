9d7cb1287b438418907e2037a57221581af68b92
Jérémy Doré <j.dore@it-students.fr>
2019-05-06 16:35:08 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './app/components/Main';
+import Note from './app/components/Note';
+
+export default class App extends React.Component{
+    render(){
+        return (
+            <Main />
+            <Note />
+        );
+    }
+}
